59d96888218bf8321989949dcbe2a635fbbf95d8
JungRan-Baek <100jran@gmail.com>
2021-05-03 16:19:24 +0900

    5.최대일수반복출력+조건종료

--- src/baek/calendar/Calendar.java
+++ src/baek/calendar/Calendar.java
@@ -4,26 +4,35 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	private static final int[] maxDayOfMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private static final int[] maxDayOfMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int get_maxDayOfMonth(int month) {
 		return maxDayOfMonth[month];
 	}
-	
-	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal 	= new Calendar();
 
-		System.out.println("반복횟수를 입력하세요.");
-		int repeat = scanner.nextInt();
-		
-		for(int i = 0; i < repeat; i++) {
-		System.out.println("월을 입력하세요.");
-		int month = scanner.nextInt();
-		
-		System.out.printf("%d월은 %d일까지 입니다.\n\n", month, cal.get_maxDayOfMonth(month));
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
+
+		while (true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print("MONTH> ");
+			int month = scanner.nextInt();
+
+			if (month == -1) {
+				break;
+			}
+			
+			if (month > 12) {
+				System.out.println();
+				continue;
+			}
+
+			System.out.printf("%d월은 %d일까지 입니다.\n\n", month, cal.get_maxDayOfMonth(month));
 		}
+		
+		System.out.println("종료");
 		scanner.close();
 	}
 }
