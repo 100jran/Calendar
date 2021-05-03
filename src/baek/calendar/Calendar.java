d92dbd75f5fcab6037846096083cfb1374cd5151
JungRan-Baek <100jran@gmail.com>
2021-05-03 16:08:51 +0900

    3.최대일수출력-배열

--- src/baek/calendar/Calendar.java
+++ src/baek/calendar/Calendar.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class Calendar {
 
+	private static final int[] maxDayOfMonth = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	public int get_maxDayOfMonth(int month) {
+		return maxDayOfMonth[month];
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("두 수를 입력하세요.");
 		Scanner scanner = new Scanner(System.in);
-		int firstNum 	= scanner.nextInt();
-		int secondNum 	= scanner.nextInt();
+		Calendar cal 	= new Calendar();
+
+		System.out.println("달을 입력하세요.");
+		int month = scanner.nextInt();
 		
-		System.out.printf("두 수의 합은 %d 입니다.", firstNum + secondNum);
+		System.out.printf("%d월은 %d일까지 입니다.", month, cal.get_maxDayOfMonth(month));
+		scanner.close();
 	}
 }
