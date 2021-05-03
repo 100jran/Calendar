36657816e84335fc9f8d4f5fccc7f423acdc9dc8
JungRan-Baek <100jran@gmail.com>
2021-05-03 16:11:56 +0900

    4.최대일수반복출력

--- src/baek/calendar/Calendar.java
+++ src/baek/calendar/Calendar.java
@@ -15,10 +15,15 @@ public class Calendar {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal 	= new Calendar();
 
-		System.out.println("달을 입력하세요.");
+		System.out.println("반복횟수를 입력하세요.");
+		int repeat = scanner.nextInt();
+		
+		for(int i = 0; i < repeat; i++) {
+		System.out.println("월을 입력하세요.");
 		int month = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 입니다.", month, cal.get_maxDayOfMonth(month));
+		System.out.printf("%d월은 %d일까지 입니다.\n\n", month, cal.get_maxDayOfMonth(month));
+		}
 		scanner.close();
 	}
 }
