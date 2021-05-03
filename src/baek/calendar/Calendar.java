26c587dc1d77d062b9827db2bf9ad0760c33b3be
JungRan-Baek <100jran@gmail.com>
2021-05-03 15:42:22 +0900

    2.a+b출력-scanner

--- src/baek/calendar/Calendar.java
+++ src/baek/calendar/Calendar.java
@@ -1,15 +1,16 @@
 package baek.calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
 		
-		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.println("---------------------");
-		System.out.println(" 1  2  3  4  5  6  7 ");
-		System.out.println(" 8  9 10 11 12 13 14 ");
-		System.out.println("15 16 17 18 19 20 21 ");
-		System.out.println("22 23 24 25 26 27 28 ");
-		System.out.println("29 30 31");
+		System.out.println("두 수를 입력하세요.");
+		Scanner scanner = new Scanner(System.in);
+		int firstNum 	= scanner.nextInt();
+		int secondNum 	= scanner.nextInt();
+		
+		System.out.printf("두 수의 합은 %d 입니다.", firstNum + secondNum);
 	}
 }
