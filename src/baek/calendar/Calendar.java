e8fc125a4bec5bca659aa843a94ab4e80fc084ab
JungRan-Baek <100jran@gmail.com>
2021-05-03 17:15:02 +0900

    8.시작일설정달력출력

--- src/baek/calendar/Calendar.java
+++ src/baek/calendar/Calendar.java
@@ -2,32 +2,59 @@ package baek.calendar;
 
 public class Calendar {
 
-	private static final int[] maxDayOfMonth 
-			= { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	private static final int[] leap_maxDayOfMonth
-			= { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	public int weekday_Start(String weekday_Str) {
 
-	public int get_maxDayOfMonth(int year, int month) {
-		
+		if (weekday_Str.equals("SU")) {
+			return 0;
+		} else if (weekday_Str.equals("MO")) {
+			return 1;
+		} else if (weekday_Str.equals("TU")) {
+			return 2;
+		} else if (weekday_Str.equals("WE")) {
+			return 3;
+		} else if (weekday_Str.equals("TH")) {
+			return 4;
+		} else if (weekday_Str.equals("FR")) {
+			return 5;
+		} else if (weekday_Str.equals("SA")) {
+			return 6;
+		} else {
+			return 0;
+		}
+
+	}
+
+	private static final int[] maxDayOfMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	private static final int[] leap_maxDayOfMonth = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int get_maxDayOfMonth(int year, int month, int weekday) {
+
 		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
 			return leap_maxDayOfMonth[month];
 		}
-		
+
 		return maxDayOfMonth[month];
 	}
 
-	public void printCalendar(int year, int month) {
-		
-		int days = get_maxDayOfMonth(year, month);
-				
+	public void printCalendar(int year, int month, int weekday) {
+
+		int days = get_maxDayOfMonth(year, month, weekday);
+		int count = 7 - weekday;
+		int delim = count < 7 ? count : 0;
+
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		
-		for(int i = 1; i <= days; i++) {
-			
+
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		for (int i = 1; i <= days; i++) {
+
 			System.out.printf("%3d", i);
-			
-			if (i % 7 == 0) {
+
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
--- src/baek/calendar/Prompt.java
+++ src/baek/calendar/Prompt.java
@@ -7,12 +7,16 @@ public class Prompt {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
+		
+		int year	= 0;
+		int month	= 0;
+		int weekday = 0;
 
 		while (true) {
 			
-			System.out.println("년도를 입력하세요.");
+			System.out.println("년도를 입력하세요.(EXIT : -1)");
 			System.out.print("YEAR> ");
-			int year = scanner.nextInt();
+			year = scanner.nextInt();
 			
 			if (year == -1) {
 				break;
@@ -20,13 +24,20 @@ public class Prompt {
 
 			System.out.println("월을 입력하세요.");
 			System.out.print("MONTH> ");
-			int month = scanner.nextInt();
+			month = scanner.nextInt();
 			
-			if (month > 12) {
+			if (month > 12 || month < 1) {
+				System.out.println("입력오류");
 				System.out.println();
 				continue;
 			}
-			cal.printCalendar(year, month);
+			
+			System.out.println("요일을 입력하세요.(SU MO TU WE TH FR SA)");
+			System.out.print("DAY> ");
+			String weekday_Str 	= scanner.next();
+			weekday			 	= cal.weekday_Start(weekday_Str);
+			
+			cal.printCalendar(year, month, weekday);
 		}
 		
 		System.out.println("종료");
