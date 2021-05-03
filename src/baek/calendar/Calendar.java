b115eb29f1e74a0d5797fd6d5c2f4768b727a142
JungRan-Baek <100jran@gmail.com>
2021-05-03 16:44:59 +0900

    7.가상의달력출력+윤년

--- src/baek/calendar/Calendar.java
+++ src/baek/calendar/Calendar.java
@@ -2,15 +2,23 @@ package baek.calendar;
 
 public class Calendar {
 
-	private static final int[] maxDayOfMonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] maxDayOfMonth 
+			= { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] leap_maxDayOfMonth
+			= { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int get_maxDayOfMonth(int month) {
+	public int get_maxDayOfMonth(int year, int month) {
+		
+		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+			return leap_maxDayOfMonth[month];
+		}
+		
 		return maxDayOfMonth[month];
 	}
 
-	public void printCalendar(int month) {
+	public void printCalendar(int year, int month) {
 		
-		int days = get_maxDayOfMonth(month);
+		int days = get_maxDayOfMonth(year, month);
 				
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
--- src/baek/calendar/Prompt.java
+++ src/baek/calendar/Prompt.java
@@ -9,19 +9,24 @@ public class Prompt {
 		Calendar cal = new Calendar();
 
 		while (true) {
+			
+			System.out.println("년도를 입력하세요.");
+			System.out.print("YEAR> ");
+			int year = scanner.nextInt();
+			
+			if (year == -1) {
+				break;
+			}
+
 			System.out.println("월을 입력하세요.");
 			System.out.print("MONTH> ");
 			int month = scanner.nextInt();
-
-			if (month == -1) {
-				break;
-			}
 			
 			if (month > 12) {
 				System.out.println();
 				continue;
 			}
-			cal.printCalendar(month);
+			cal.printCalendar(year, month);
 		}
 		
 		System.out.println("종료");
